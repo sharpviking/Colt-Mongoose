122f9199499f436951118528acc5effb146f896a
Abhishek Anand <anandabhishek3607@gmail.com>
2023-07-04 13:49:12 +0530

    mongoose model instance methods

--- product.js
+++ product.js
@@ -8,17 +8,69 @@ mongoose.connect("mongodb+srv://sharpviking:l9a53607@cluster0.0maezhz.mongodb.ne
 const productSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        maxLength: 20
     },
     price: {
         type: Number,
-        required: true
+        required: true,
+        min: [0, 'price must be positive ya kalia!']
+    },
+    onSale: {
+        type: Boolean,
+        default: false
+    },
+    categories: {
+        type: String,
+
+    },
+    qty: {
+        online: {
+            type: Number,
+            default: 0
+        },
+        inStore: {
+            type: Number,
+            default: 0
+        }
+    },
+    size: {
+        type: String,
+        enum: ['S', 'M', 'L']
     }
 });
 
+productSchema.methods.toggleOnSale = function () {
+    this.onSale = !this.onSale;
+    return this.save();
+}
+
+
+productSchema.methods.addCategory = function (newCat) {
+    this.categories.push(newCat);
+    return this.save();
+
+}
+
+productSchema.methods.greet = function () {
+    console.log("Hello!!! HI!! HOWDY!!")
+    console.log(`- frm ${this.name}`)
+}
+
+const findProduct = async () => {
+    const foundproduct = await Product.findOne({ name: 'Street Bike' })
+    foundproduct.greet();
+    await foundproduct.toggleOnSale();
+    console.log(foundproduct)
+    await foundproduct.addCategory('Outdoors');
+    console.log(foundproduct);
+}
+
+findProduct();
+
 const Product = mongoose.model('Product', productSchema);
 
-const bike = new Product({ name: 'Street Bike', price: 115000 })
+const bike = new Product({ name: 'Street Bike', price: 115000, categories: ['City driving', 'highway riding'] })
 bike.save()
     .then(data => {
         console.log("It Worked!")
